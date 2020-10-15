d6cc6d48e122b0b2b0405fb33feaef10c8c869fa
dstorrs <dstorrs.public@gmail.com>
2020-10-15 14:41:40 -0400

    updated info.rkt with new deps and version number

--- info.rkt
+++ info.rkt
@@ -2,8 +2,8 @@
 
 (define collection "trie")
 (define deps '("base" "handy" "struct-plus-plus"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "sandbox-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "sandbox-lib" "handy"))
 (define scribblings '(("scribblings/trie.scrbl" ())))
 (define pkg-desc "Implements the trie data structure")
-(define version "0.1")
+(define version "0.2")
 (define pkg-authors '("David K. Storrs"))
